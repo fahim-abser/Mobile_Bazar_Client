94dbbe64ba7b9c193897bd6a3f24d09e753df4ee
fahim-abser <fahimabser0123@gmail.com>
2022-11-27 03:03:26 +0600

    private route setup complete

--- src/Router/Routes/Routes.js
+++ src/Router/Routes/Routes.js
@@ -1,10 +1,32 @@
 import { createBrowserRouter } from "react-router-dom";
+import Blog from "../../Components/Blog/Blog";
+import Home from "../../Components/Home/Home";
+import Login from "../../Components/Login/Login";
+import SignUp from "../../Components/SignUp/SignUp";
 import Main from "../../Layout/Main";
 
 const router = createBrowserRouter([
     {
         path:'/',
-        element:<Main></Main>
+        element:<Main></Main>,
+        children:[
+            {
+                path: '/',
+                element: <Home></Home>
+              },
+            {
+                path: '/login',
+                element: <Login></Login>
+              },
+              {
+                path: '/signup',
+                element: <SignUp></SignUp>
+              },
+              {
+                path: '/blog',
+                element: <Blog></Blog>
+              },
+        ]
     }
 ])
 
