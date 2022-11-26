4092376f143dadfc28fde962514b1df83cc9395b
fahim-abser <fahimabser0123@gmail.com>
2022-11-27 03:04:26 +0600

    sign up  complete

--- src/Components/SignUp/SignUp.js
+++ src/Components/SignUp/SignUp.js
@@ -1,9 +1,44 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const SignUp = () => {
     return (
-        <div>
+        <div className="hero w-full my-20">
             
+            <div className="hero-content grid gap-20 md:grid-cols-2 flex-col lg:flex-row">
+                <div className="text-center lg:text-left">
+                <h1 className="text-5xl font-bold">Don't have account?</h1>
+                     <p className="py-6 text-3xl">Please SignUp here!</p>
+                </div>
+                <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100 py-20">
+                    <h1 className="text-5xl text-center font-bold">Sign Up</h1>
+                    <form  className="card-body">
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Name</span>
+                            </label>
+                            <input type="text" name='name' placeholder="Your Name" className="input input-bordered" />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Email</span>
+                            </label>
+                            <input type="text" name='email' placeholder="email" className="input input-bordered" required/>
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Password</span>
+                            </label>
+                            <input type="password" name='password' placeholder="password" className="input input-bordered" required/>
+                            
+                        </div>
+                        <div className="form-control mt-6">
+                            <input className="btn btn-primary" type="submit" value="Sign Up" />
+                        </div>
+                    </form>
+                    <p className='font-bold text-center'>Already have an account? <Link className='font-bold' to="/login">Log in</Link> </p>
+                </div>
+            </div>
         </div>
     );
 };
