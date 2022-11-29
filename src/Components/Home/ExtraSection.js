623983da527a06ac750d80d22188c11debe8a48e
fahim-abser <fahimabser0123@gmail.com>
2022-11-29 15:55:10 +0600

    extra section added

--- src/Components/Home/ExtraSection.js
+++ src/Components/Home/ExtraSection.js
@@ -3,7 +3,26 @@ import React from 'react';
 const ExtraSection = () => {
     return (
 
-        <div></div>
+        <div className='flex px-4 p-4'>
+            <div className="card w-96 bg-base-200 shadow-xl">
+                <div className="card-body">
+                    <h2 className="card-title">SUPPORT 24/7</h2>
+                    <p>Contact us 24 hours, 7 das a week.</p>
+                </div>
+            </div>
+            <div className="card w-96 bg-base-200 shadow-xl">
+                <div className="card-body">
+                    <h2 className="card-title">30 DAS RETURN</h2>
+                    <p>Single return it within 24 das for an exchange.</p>
+                </div>
+            </div>
+            <div className="card w-96 bg-base-200 shadow-xl">
+                <div className="card-body">
+                    <h2 className="card-title">FREE SHIPPING</h2>
+                    <p>Free shipping on all order or order above 10k</p>
+                </div>
+            </div>
+        </div>
     );
 };
 
